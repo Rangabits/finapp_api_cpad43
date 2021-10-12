3d408c25addf0488af29045181bb5725c7a44974
Rangabits <2020mt93596@wilp.bits-pilani.ac.in>
2021-10-12 19:44:09 +0530

    EMI_Cal

--- controllers/calculator.js
+++ controllers/calculator.js
@@ -105,7 +105,60 @@ module.exports = {
         }
         return output;
     },
-    
+
+    // Determine Rate of interest and tenure based on loan category
+        emi_rate: function (l_type) {
+                
+            var output = {
+                "Rate_Pers_Loan": 10,
+                "Rate_Edu_Loan": 6,
+                "Rate_Home_Loan": 11,
+                "Rate_Auto_Loan": 12,
+            }
+            return output;
+        },
+
+     // Calculate EMI 
+     emi_cal: function (l_type, loan_amnt, i_rate, i_time) {
+        
+        var loan_type;
+        switch (l_type) {
+            case 'P':
+                loan_type = "Personal Loan"
+                break;
+            case 'E':
+                loan_type = "Education Loan"
+                break;
+            case 'H':
+                loan_type = "Home Loan"
+                break;
+            case 'A':
+                loan_type = "Automobile Loan"
+                break;
+
+        }
+
+        var rate = i_rate / 1200 ;
+        
+        var n = i_time * 12;
+        
+        var rpown = Math.pow( (1 + rate), n);
+        
+		var num = ( loan_amnt * rate ) * rpown;
+        
+		var den = rpown - 1;
+        
+		var month_emi = Math.round( num / den );
+        
+        var output = {
+            "Loan_Type": loan_type,
+            "Principle_Amount": loan_amnt,
+            "Interest_Rate": i_rate,
+            "Tenure_Years": i_time,
+            "Cal_EMI": month_emi
+        }
+        return output;
+    },
 // Similalry functions need to be built for other calculators
 
 };
--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.get('/inv_prop/:salary/:age', finserv.inv_prop);
 // getting investment proposal details
 app.get('/inv_cal/:inv_amnt/:age', finserv.inv_cal);
 
+// getting loan emi rates
+app.get('/emi_rate/:l_type', finserv.emi_rate);
+
+// getting loan emi details
+app.get('/emi_cal/:l_type/:p_amount/:i_rate/:i_time', finserv.emi_cal);
+
 // Similar calls to be made for other calculators as per need
 
 app.use(function(req, res) {
